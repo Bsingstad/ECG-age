48b56d926c80b4636d5aa646f5777a52ab71a334
Singstad90 <bjorn_sing@hotmail.com>
2022-06-14 19:15:14 +0200

    Create helpers.py

---
+++ src/helpers.py
@@ -0,0 +1,12 @@
+from scipy.io import loadmat
+import numpy as np
+import os
+
+def load_challenge_data(filename):
+    x = loadmat(filename)
+    data = np.asarray(x['val'], dtype=np.float64)
+    new_file = filename.replace('.mat','.hea')
+    input_header_file = os.path.join(new_file)
+    with open(input_header_file,'r') as f:
+        header_data=f.readlines()
+    return data, header_data
